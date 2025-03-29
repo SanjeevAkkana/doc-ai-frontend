941493074f5288a5b36562f36e2c1c387c4f3b91
SanjeevAkkana <asanjeevroyal@gmail.com>
2025-03-29 22:39:41 +0530

    Final Commit

--- components/shared/ChatBox.js
+++ components/shared/ChatBox.js
@@ -23,7 +23,7 @@ const ChatBox = ({ onClose }) => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3001/chat/health-chat-bot", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/chat/health-chat-bot`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ query }),
